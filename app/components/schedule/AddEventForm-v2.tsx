fd346b644eeb4546b8ec0bfabc237e86a596dd11
tkatter <tjkatter@gmail.com>
2025-05-30 10:54:18 -0500

    added debug button to form

--- app/components/schedule/AddEventForm-v2.tsx
+++ app/components/schedule/AddEventForm-v2.tsx
@@ -2,6 +2,7 @@ import { useAppForm } from "~/components/form/appForm";
 import FieldInfo from "../form/FieldInfo";
 import { formOpts } from "~/lib/tanstack/form-utils";
 import { log } from "~/lib/utils";
+import { Button } from "../ui/button";
 
 function AddEventForm() {
   const form = useAppForm({
@@ -12,129 +13,139 @@ function AddEventForm() {
   });
 
   return (
-    <form
-      onSubmit={(e) => {
-        e.preventDefault();
-        e.stopPropagation();
-        form.handleSubmit();
-      }}
-      className="flex flex-col gap-6"
-    >
-      <form.AppField
-        name="name"
-        children={(field) => (
-          <>
-            <field.InputField label="Event Name" />
-            <FieldInfo field={field} />
-          </>
-        )}
-      />
-      <form.AppField
-        name="description"
-        children={(field) => (
-          <>
-            <field.InputField
-              label="Description"
-              type="area"
-              placeholder="Event description"
-            />
-            <FieldInfo field={field} />
-          </>
-        )}
-      />
-      <div className="flex items-center w-full gap-4">
+    <>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          e.stopPropagation();
+          form.handleSubmit();
+        }}
+        className="flex flex-col gap-6"
+      >
         <form.AppField
-          name="status"
+          name="name"
           children={(field) => (
-            <div className="w-full">
-              <field.SelectField
-                label="Status"
-                placeholder="Status"
-                options={[
-                  "pending",
-                  "confirmed",
-                  "in-progress",
-                  "completed",
-                  "cancelled",
-                ]}
-              />
+            <>
+              <field.InputField label="Event Name" />
               <FieldInfo field={field} />
-            </div>
+            </>
           )}
         />
         <form.AppField
-          name="eventType"
+          name="description"
           children={(field) => (
-            <div className="w-full">
-              <field.SelectField
-                label="Type"
-                placeholder="Event Type"
-                options={["job", "meeting", "event", "appointment"]}
+            <>
+              <field.InputField
+                label="Description"
+                type="area"
+                placeholder="Event description"
               />
               <FieldInfo field={field} />
-            </div>
+            </>
           )}
         />
-      </div>
-      <form.AppField
-        name="startDate"
-        children={(field) => (
-          <>
-            <field.DatePickerField className="w-full" />
-            <FieldInfo field={field} />
-          </>
-        )}
-      />
-      <form.AppField
-        name="location.address"
-        children={(field) => (
-          <>
-            <field.InputField label="Street Address" />
-            <FieldInfo field={field} />
-          </>
-        )}
-      />
-      <form.AppField
-        name="location.city"
-        children={(field) => (
-          <>
-            <field.InputField label="City" />
-            <FieldInfo field={field} />
-          </>
-        )}
-      />
-      <form.AppField
-        name="location.zip"
-        children={(field) => (
-          <>
-            <field.InputField label="Zipcode" />
-            <FieldInfo field={field} />
-          </>
-        )}
-      />
-      <form.AppField
-        name="location.state"
-        children={(field) => (
-          <>
-            <field.InputField label="State" />
-            <FieldInfo field={field} />
-          </>
-        )}
-      />
-      <form.AppField
-        name="notes"
-        children={(field) => (
-          <>
-            <field.InputField
-              label="Notes"
-              type="area"
-              placeholder="Other details or information"
-            />
-            <FieldInfo field={field} />
-          </>
-        )}
-      />
-    </form>
+        <div className="flex items-center w-full gap-4">
+          <form.AppField
+            name="status"
+            children={(field) => (
+              <div className="w-full">
+                <field.SelectField
+                  label="Status"
+                  placeholder="Status"
+                  options={[
+                    "pending",
+                    "confirmed",
+                    "in-progress",
+                    "completed",
+                    "cancelled",
+                  ]}
+                />
+                <FieldInfo field={field} />
+              </div>
+            )}
+          />
+          <form.AppField
+            name="eventType"
+            children={(field) => (
+              <div className="w-full">
+                <field.SelectField
+                  label="Type"
+                  placeholder="Event Type"
+                  options={["job", "meeting", "event", "appointment"]}
+                />
+                <FieldInfo field={field} />
+              </div>
+            )}
+          />
+        </div>
+        <form.AppField
+          name="startDate"
+          children={(field) => (
+            <>
+              <field.DatePickerField className="w-full" />
+              <FieldInfo field={field} />
+            </>
+          )}
+        />
+        <form.AppField
+          name="location.address"
+          children={(field) => (
+            <>
+              <field.InputField label="Street Address" />
+              <FieldInfo field={field} />
+            </>
+          )}
+        />
+        <form.AppField
+          name="location.city"
+          children={(field) => (
+            <>
+              <field.InputField label="City" />
+              <FieldInfo field={field} />
+            </>
+          )}
+        />
+        <form.AppField
+          name="location.zip"
+          children={(field) => (
+            <>
+              <field.InputField label="Zipcode" />
+              <FieldInfo field={field} />
+            </>
+          )}
+        />
+        <form.AppField
+          name="location.state"
+          children={(field) => (
+            <>
+              <field.InputField label="State" />
+              <FieldInfo field={field} />
+            </>
+          )}
+        />
+        <form.AppField
+          name="notes"
+          children={(field) => (
+            <>
+              <field.InputField
+                label="Notes"
+                type="area"
+                placeholder="Other details or information"
+              />
+              <FieldInfo field={field} />
+            </>
+          )}
+        />
+      </form>
+      <Button
+        onClick={(e) => {
+          e.preventDefault();
+          log(form.state.values);
+        }}
+      >
+        Debug
+      </Button>
+    </>
   );
 }
 
