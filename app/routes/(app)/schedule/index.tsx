a450c7fa1a3c6d2644109956ca293e7c16d6fdc6
tkatter <tjkatter@gmail.com>
2025-06-04 17:01:09 -0500

    sorted imports in schedule route index

--- app/routes/(app)/schedule/index.tsx
+++ app/routes/(app)/schedule/index.tsx
@@ -1,13 +1,13 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { scheduleQueries } from "~/lib/queries";
-import SchedulePage from "~/routes/(app)/schedule/-pages/SchedulePage";
 import LoadingSpinner from "~/components/ui/LoadingSpinner";
 import { ScheduleProvider } from "~/context/ScheduleContext";
+import { scheduleQueries } from "~/lib/queries";
+import SchedulePage from "~/routes/(app)/schedule/-pages/SchedulePage";
 
 export const Route = createFileRoute("/(app)/schedule/")({
   loader: async ({ context: { queryClient } }) => {
     await queryClient.ensureQueryData(
-      scheduleQueries.getAllScheduleItemsOpts()
+      scheduleQueries.getAllScheduleItemsOpts(),
     );
   },
 
